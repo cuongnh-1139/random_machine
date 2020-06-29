2f6176f700fd51220777e58c36b936d9f7884435
Cuong Nguyen Huy (cuongnh-1139) <nguyen.huy.cuong@sun-asterisk.com>
2020-06-29 10:06:39 +0700

    add audio control

--- build/script.js
+++ build/script.js
@@ -1,56 +1,85 @@
+// initial declaration
 const minInput = document.querySelector('#min');
-      const maxInput = document.querySelector('#max');
-      const resultDiv = document.querySelector('.result');
-      const audioControl = document.querySelector('audio');
-      const button = document.querySelector('button');
-      let infinityTimer;
-      let timeoutTimer;
-      minInput.value = 1;
-      maxInput.value = 100;
-      function getValue() {
-        let min = Number(minInput.value) || 1;
-        let max = Number(maxInput.value) || 100;
-        if (min > max) {
-          min = 1;
-          max = 100;
-        }
-        minInput.value = min;
-        maxInput.value = max;
-        return { min, max };
-      }
+const maxInput = document.querySelector('#max');
+const resultDiv = document.querySelector('div.result');
+const audioControl = document.querySelector('audio#background');
+const toneControl = document.querySelector('audio#tone');
+const startButton = document.querySelector('button#start');
+const resetButton = document.querySelector('button#reset');
+const speakerImg = document.querySelector('button#mute img');
+let infinityTimer;
+let timeoutTimer;
 
-      function disabledElements() {
-        button.setAttribute('disabled', true);
-      }
+// initial values
+minInput.value = 1;
+maxInput.value = 100;
+audioControl.loop = true;
+audioControl.volume = 0.6;
+audioControl.play();
 
-      function enableElements() {
-        button.removeAttribute('disabled');
-      }
+function getValue() {
+  let min = Number(minInput.value) || 1;
+  let max = Number(maxInput.value) || 100;
+  if (min > max) {
+    min = 1;
+    max = 100;
+  }
+  minInput.value = min;
+  maxInput.value = max;
+  return { min, max };
+}
 
-      function showResult() {
-        const { min, max } = getValue();
-        const result = Math.round(Math.random() * (max + 1 - min) + min);
-        resultDiv.innerHTML = result + '';
-      }
+function disabledElements() {
+  startButton.setAttribute('disabled', true);
+  resetButton.setAttribute('disabled', true);
+}
 
-      function showLoading() {
-        clearInterval(infinityTimer);
-        audioControl.currentTime = Math.round(Math.random() * 20);
-        audioControl.play();
+function enableElements() {
+  startButton.removeAttribute('disabled');
+  resetButton.removeAttribute('disabled');
+}
 
-        infinityTimer = setInterval(() => {
-          showResult();
-        }, 30);
+function showResult() {
+  const { min, max } = getValue();
+  const result = Math.round(Math.random() * (max + 1 - min) + min);
+  resultDiv.innerHTML = result + '';
+}
 
-        setTimeout(() => {
-          clearInterval(infinityTimer);
-          audioControl.pause();
-          audioControl.currentTime = 0;
-          enableElements();
-        }, 3000);
-      }
+function showLoading() {
+  clearInterval(infinityTimer);
+  audioControl.pause();
+  toneControl.play();
 
-      function handleClick() {
-        disabledElements();
-        showLoading();
-      }
+  infinityTimer = setInterval(() => {
+    showResult();
+  }, 30);
+
+  setTimeout(() => {
+    clearInterval(infinityTimer);
+    toneControl.pause();
+    toneControl.currentTime = 0;
+    audioControl.play();
+    enableElements();
+  }, 4000);
+}
+
+function handleClick() {
+  disabledElements();
+  showLoading();
+}
+
+function handleReset() {
+  minInput.value = 1;
+  maxInput.value = 100;
+  resultDiv.innerHTML = '--';
+}
+
+function toggleBackground() {
+  const current = audioControl.muted;
+  audioControl.muted = !current;
+  toneControl.muted = !current;
+  speakerImg.setAttribute(
+    'src',
+    `static/${current ? 'speaker' : 'speaker-mute'}.svg`
+  );
+}
